c38e45057341904f19b473abee27a1182da0b9b8
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-25 23:11:23 +0530

    mongoose connected

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+
+//setting up static file
+app.use(express.static('./assets'))
 
 app.use(expressLayouts);
+//extract styles and scripts from subpages into layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 //use express router
 app.use('/', require('./routes'));
