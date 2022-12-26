49d32dcaca0233517f78b7a206af0f295ea39555
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-26 14:08:23 +0530

    created userSchema and rendered sign up and signin pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,28 @@
 module.exports.profile = function(req, res){
     return res.render('user_profile',{
-        title: 'User'
+        title: 'User Profile'
     })
+}
+
+//render the signup page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: 'Codeial || SignUp'
+    })
+}
+//render the signin page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: 'Codeial || SignIn'
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req, res){
+
+}
+
+//for signin
+module.exports.createSession = function(req, res){
+    
 }
