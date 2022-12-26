936e14ec75eee5a5756eae3c10e03a063f9f95f3
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-26 17:20:07 +0530

    sign up created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(req, res){
     return res.render('user_profile',{
         title: 'User Profile'
@@ -19,10 +22,25 @@ module.exports.signIn = function(req, res){
 
 //get the sign up data
 module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user anad signing up'); return}
 
+        if(!user){
+            user.create(req.body, function(err, user){
+                if(err){console.log('error in creating up user'); return}
+
+                return res.redirect('./users/sign-in')
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
 
 //for signin
 module.exports.createSession = function(req, res){
-    
+
 }
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser  = require('body-parser');
 const port = 8000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+  })); 
+
 //setting up static file
-app.use(express.static('./assets'))
+app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 //extract styles and scripts from subpages into layouts
