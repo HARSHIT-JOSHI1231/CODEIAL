5fd31db64ba8e1aa1a6b63ebc9b89d98fe095f82
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-27 17:56:06 +0530

    created sign-out page and updated _header in views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -56,9 +56,16 @@ module.exports.create = function(req, res){
     
     }) 
 }
-
-
 //for signin
 module.exports.createSession = function(req, res){
     return res.redirect('/');
 }
+
+module.exports.destroySession = function(req, res, next){
+    req.logout(function(err) {
+        if (err) { 
+          return next(err); 
+          }
+        res.redirect('/');
+      });
+};
