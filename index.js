364b39fbf1f7f3cbf2b6106f3c66bf6595e5e101
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-29 15:48:49 +0530

    added sass/scss

--- index.js
+++ index.js
@@ -6,12 +6,34 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+
 //used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy')
 const MongoStore = require('connect-mongo');
 
+const sass = require('node-sass');
+
+/* app.use(sass({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+
+})) */
+
+
+/* app.use(sassMiddleware({
+    src:'/assets/scss',
+    dest:'/assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'/css'
+
+
+})); */
 
 
 
