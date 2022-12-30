73dcb95c61d0a2df47ac10f109da101afe9c9c63
Harshit Joshi <joshiharshit1231@gmail.com>
2022-12-30 15:49:18 +0530

    check Authentication on creating post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,19 @@
+const Post = require('../models/post');
+const { prependOnceListener } = require('../models/user');
+
 module.exports.home = function(req, res){
-    res.cookie('user_id', 25);
-    return res.render('home',{
-        title: 'Home',
+    //res.cookie('user_id', 25);
+    /* Post.find({}, function(err, posts){
+        return res.render('home',{
+            title: 'Codeial | Home',
+            posts: posts
+        });
+    }); */
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err, post){
+        return res.render('home',{
+            title: 'Codeial | Home',
+            posts: post
+        });
     })
-}
+    }
